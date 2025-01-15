7a0cc14c5a6417ca0316c3df96e3bebc216be3c0
bmthd <jougennotuki67@gmail.com>
2025-01-15 21:58:08 +0900

    add:react compiler

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    reactCompiler: true,
+    typedEnv: true,
+    // typedRoutes: true,
+  },
 };
 
 export default nextConfig;
