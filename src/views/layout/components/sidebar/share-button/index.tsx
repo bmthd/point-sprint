816bf29937b1baf845573c51c3564b373088866a
bmthd <jougennotuki67@gmail.com>
2025-01-16 22:59:22 +0900

    fix:ボタンのスタイル

--- src/views/layout/components/sidebar/share-button/index.tsx
+++ src/views/layout/components/sidebar/share-button/index.tsx
@@ -3,7 +3,7 @@ import { H } from "@/ui/structure/h";
 import { faFacebook, faLine, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@yamada-ui/fontawesome";
 import { ClipboardCopyIcon } from "@yamada-ui/lucide";
-import { Card, HStack, IconButton, StyleProperties } from "@yamada-ui/react";
+import { Card, HStack, IconButton, IconButtonProps } from "@yamada-ui/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ReactNode, useEffect, useState } from "react";
@@ -25,40 +25,45 @@ export const ShareButtonContainer = () => {
 
   const buttons = [
     {
+      label: "Twitter",
       href: `https://twitter.com/intent/tweet?text=${encodeURIComponent(
         title,
       )}&url=${encodeURIComponent(pathname)}`,
       icon: <FontAwesomeIcon icon={faXTwitter} />,
-      label: "Twitter",
-      style: { bg: "white" },
+      bg: "blackAlpha.900",
+      _hover: { bg: "blackAlpha.800" },
     },
     {
+      label: "Facebook",
       href: `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(pathname)}`,
       icon: <FontAwesomeIcon icon={faFacebook} />,
-      label: "Facebook",
-      style: { backgroundColor: "#1877f2" },
+      // style: { backgroundColor: "#1877f2" },
+      colorScheme: "blue",
     },
     {
+      label: "LINE",
       href: `https://social-plugins.line.me/lineit/share?url=${encodeURIComponent(pathname)}`,
       icon: <FontAwesomeIcon icon={faLine} />,
-      label: "LINE",
-      style: { backgroundColor: "#00b900" },
+      // style: { backgroundColor: "#00b900" },
+      colorScheme: "green",
     },
-  ] as const satisfies { href: string; icon: ReactNode; label: string; style?: StyleProperties }[];
+  ] as const satisfies ({ label: string; href: string; icon: ReactNode } & IconButtonProps)[];
 
   return (
     <Card>
       <H>このページをシェア</H>
       <HStack>
-        {buttons.map(({ href, icon, label, style }) => (
-          <Link href={href} key={label}>
-            <IconButton icon={icon} {...style} />
+        {buttons.map(({ href, label, ...props }) => (
+          <Link key={label} href={href}>
+            <IconButton {...props} />
           </Link>
         ))}
         <IconButton
           icon={<ClipboardCopyIcon />}
           onClick={copyToClipboard}
-          style={{ backgroundColor: "#333" }}
+          aria-label="クリップボードにコピー"
+          bg="blue.900"
+          _hover={{ bg: "blue.800" }}
         />
       </HStack>
     </Card>
